b20c47284ad30e5da94737f8d52e42862082c036
Matthew Giglio <matthew.giglio@duke.edu>
2022-01-24 19:20:36 -0500

    Created Coordinate class to represent positions in simulation grid

---
+++ src/main/java/Model/Coordinate.java
@@ -0,0 +1,17 @@
+package Model;
+
+public class Coordinate {
+
+  private int row;
+  private int column;
+
+  public Coordinate(int row, int column) {
+    this.row = row;
+    this.column = column;
+  }
+
+  public int getRow() {return row;}
+
+  public int getColumn() {return column;}
+
+}
