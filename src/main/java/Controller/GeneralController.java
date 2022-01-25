fbc5cfd246453c0ca0651cdb2009f8f5837437d6
Saad Lahrichi <sl636@duke.edu>
2022-01-25 02:13:53 -0500

    ADD: Sample XML fire for SpreadingFire and basic Controller class

---
+++ src/main/java/Controller/GeneralController.java
@@ -0,0 +1,25 @@
+package Controller;
+
+public abstract class GeneralController{
+
+
+  //List of all possible simulations. Set the type to SimulationXml to avoid confusion with the
+  //Model.Simulation objects as SimulationXml only returns the XML file of each Simulation.
+
+  private static final List<SimulationXml> = POSSIBLE_SIMULATIONS = List.of(
+      new GameOfLifeXml(),
+      new SpreadingOfFireXml(),
+      new SegregationXml(),
+      new PredatorPrey(),
+      new Percolation()
+      );
+
+
+  // This is similar to the Roulette code. Here I assume that the View
+  // would prompt the user to pick a Simulation type, and pass to pickSimulation
+  // the corresponding number in order of apparition in the project's description.
+
+  private SimulationXml pickSimulation(int whichSimulation){
+    return POSSIBLE_SIMULATIONS.get(whichSimulation);
+  }
+}
---
+++ src/main/java/Controller/XMLParser.java
@@ -0,0 +1,14 @@
+package Controller;
+
+public XMLParser(){
+
+
+  public write(){
+
+    }
+
+   public read(){
+
+    }
+
+}
