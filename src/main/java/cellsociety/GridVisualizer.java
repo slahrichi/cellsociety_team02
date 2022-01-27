b5a08d95da982bbf66bdfd6ad616abc60be11882
Luka Mdivani <lm378@duke.edu>
2022-01-26 22:33:17 -0500

    added GridVisualizer abstract class

--- src/main/java/cellsociety/GridVisualizer.java
+++ src/main/java/cellsociety/GridVisualizer.java
@@ -1,5 +1,29 @@
 package cellsociety;
 
-public class GridVisualizer {
+import javafx.scene.Group;
+import javafx.scene.shape.Shape;
+
+public abstract class GridVisualizer {
+  protected int gridWidth;
+  protected int gridHeight;
+  protected int numRows;
+  protected int numColumns;
+
+  public GridVisualizer(int width, int height,int numberOfRows,int numberOfColumns){
+    this.gridWidth=width;
+    this.gridHeight=height;
+    this.numRows = numberOfRows;
+    this.numColumns = numberOfColumns;
+
+  }
+
+  protected abstract void calculateCellSize() ;
+
+  public abstract Group makeRoot() ;
+
+  protected abstract Group arrangeCells();
+
+  protected abstract Shape createCell(int xPos, int yPos) ;
+
 
 }
