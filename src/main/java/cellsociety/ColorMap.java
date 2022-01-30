14a5ac5973f72ce8bb98b9af19a2ab002ee98e95
Luka Mdivani <lm378@duke.edu>
2022-01-30 02:22:32 -0500

    bug fix

--- src/main/java/cellsociety/ColorMap.java
+++ src/main/java/cellsociety/ColorMap.java
@@ -15,12 +15,12 @@ public class ColorMap {
     COLOR_MAP.put("TREE", Color.GREEN);
     COLOR_MAP.put("BURNING", Color.CRIMSON);
 
-    COLOR_MAP.put("PERCOLATED,", Color.YELLOW);
-    COLOR_MAP.put("OPEN,", Color.WHITESMOKE);
-    COLOR_MAP.put("BLOCKED,", Color.DARKORANGE);
+    COLOR_MAP.put("PERCOLATED", Color.YELLOW);
+    COLOR_MAP.put("OPEN", Color.ROSYBROWN);
+    COLOR_MAP.put("BLOCKED", Color.DARKORANGE);
 
-    COLOR_MAP.put("ALIVE,", Color.BLUE);
-    COLOR_MAP.put("DEAD,", Color.GRAY);
+    COLOR_MAP.put("ALIVE", Color.PURPLE);
+    COLOR_MAP.put("DEAD", Color.GRAY);
 
     COLOR_MAP.put("REP", Color.RED);
     COLOR_MAP.put("DEM", Color.BLUE);
--- src/main/java/cellsociety/Main.java
+++ src/main/java/cellsociety/Main.java
@@ -2,10 +2,7 @@ package cellsociety;
 
 import Controller.XMLParserPlayground;
 import Model.Coordinate;
-import Model.Segregation;
 import Model.Simulation;
-import Model.SpreadingFire;
-import java.util.Arrays;
 import java.util.HashMap;
 import javafx.application.Application;
 import javafx.stage.Stage;
@@ -25,7 +22,7 @@ public class Main extends Application {
   public static final int SIZE_HORIZONTAL = 725;
   public static final int SIZE_VERTICAL = 575;
   public static String  language = "English";
-  public static final String DEFAULT_FILE_PATH = "doc/SpreadingFire.xml";
+  public static final String DEFAULT_FILE_PATH = "doc/GameOfLifeBlinker.xml";
   private SimulationVisualizer visualizer;
   private HashMap<Coordinate, Integer> setup;
   private HashMap<String, String> data;
@@ -35,18 +32,6 @@ public class Main extends Application {
   private String currentFilePath;
   private XMLParserPlayground parser;
 
-  private HashMap<Coordinate, Integer> setupSegregation(int[][] grid) {
-    HashMap<Coordinate, Integer> map = new HashMap<>();
-    for (int i = 0; i < grid.length; i++) {
-      for (int j = 0; j < grid[0].length; j++) {
-        Coordinate p = new Coordinate(i, j);
-        map.put(p, grid[i][j]);
-      }
-    }
-    return map;
-  }
-
-
   /**
    * Initialize what will be displayed.
    */
@@ -54,11 +39,7 @@ public class Main extends Application {
   public void start(Stage stage) throws Exception {
 
 
-   //int[][] grid = {{2, 2, 1, 2, 1}, {0, 1, 1, 1, 1}, {2, 2, 0, 0, 0},
-   //    {2, 1, 2, 2, 2}, {2, 1, 1, 0, 1}};
-   // setup = setupSegregation(grid);
-
-   parser = new XMLParserPlayground();
+    parser = new XMLParserPlayground();
     currentFilePath=DEFAULT_FILE_PATH;
     extractDataStartSimulation(DEFAULT_FILE_PATH);
 
@@ -70,11 +51,7 @@ public class Main extends Application {
     data = parser.parseXML(filePath);
 
     String grid = data.get("grid");
-    String allNumbers = grid.replaceAll("[^0-9]", "");
-    //// source: https://stackoverflow.com/questions/42546052/take-a-string-and-turn-it-into-a-2d-array-java
-    int[][] numbers = Arrays.stream(allNumbers.split("(?<=\\G.{10})"))
-        .map(s -> (Arrays.stream(s.split("(?<=\\G.{1})")).mapToInt(Integer::parseInt).toArray()))
-        .toArray(int[][]::new);
+
     test = parser.createSimulation(data);
     getNumberOfColumnAndRow();
   }
