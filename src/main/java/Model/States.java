a28f78c6e71bb8284b7cde00cdddafdc04a9551a
Matthew Giglio <matthew.giglio@duke.edu>
2022-02-03 20:00:31 -0500

    Removed extraneous methods from States

--- src/main/java/Model/States.java
+++ src/main/java/Model/States.java
@@ -6,13 +6,6 @@ public class States {
     TREE, //GREEN
     BURNING, //RED
     EMPTY;
-
-    public int stateToInt(Enum state) {
-      if (state.equals(TREE)) return 0;
-      else if (state.equals(BURNING)) return 1;
-      else if (state.equals(EMPTY)) return 2;
-      else throw new IllegalArgumentException("Not a valid state");
-      }
     }
 
 
@@ -20,64 +13,29 @@ public class States {
     OPEN, //WHITE
     PERCOLATED, //BLUE
     BLOCKED; //BLACK
-    public int stateToInt(Enum state) {
-      if (state.equals(OPEN)) return 0;
-      else if (state.equals(PERCOLATED)) return 1;
-      else if (state.equals(BLOCKED)) return 2;
-      else throw new IllegalArgumentException("Not a valid state");
-    }
   }
 
   public enum GameOfLife {
     DEAD, //
     ALIVE;
-    public int stateToInt(Enum state) {
-      if (state.equals(DEAD)) return 0;
-      else if (state.equals(ALIVE)) return 1;
-      else throw new IllegalArgumentException("Not a valid state");
-    }
   }
 
   public enum Segregation {
     EMPTY,
     REP,
     DEM;
-    public int stateToInt(Enum state) {
-      if (state.equals(EMPTY)) return 0;
-      else if (state.equals(REP)) return 1;
-      else if (state.equals(DEM)) return 2;
-      else throw new IllegalArgumentException("Not a valid state");
-    }
   }
 
   public enum WaTor {
     EMPTY,
     FISH,
     SHARK;
-    public int stateToInt(Enum state) {
-      if (state.equals(EMPTY)) return 0;
-      else if (state.equals(FISH)) return 1;
-      else if (state.equals(SHARK)) return 2;
-      else throw new IllegalArgumentException("Not a valid state");
-    }
   }
 
   public enum RockPaperScissors {
     ROCK,
     PAPER,
     SCISSORS;
-
-    public static Enum getWinningNeighbor(Enum currentState) {
-      if (currentState == SCISSORS) {
-        return ROCK;
-      }
-      else if (currentState == PAPER) {
-        return SCISSORS;
-      }
-      else {
-        return PAPER;
-        }
-    }
   }
 
   public enum FallingSand {
