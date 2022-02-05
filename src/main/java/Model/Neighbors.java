3a850b74069df7aa5699bc09c64aec93795c2f3d
Matthew Giglio <matthew.giglio@duke.edu>
2022-02-05 11:44:09 -0500

    Adjusted row/column ordering for square, hexagonal, and triangular

--- src/main/java/Model/Neighbors.java
+++ src/main/java/Model/Neighbors.java
@@ -4,11 +4,11 @@ import java.util.List;
 
 public class Neighbors {
 
-  private static final int[] SQUARE_ROW = {-1, 1, 0, 0, 1, 1, -1, -1};
+  private static final int[] SQUARE_ROW = {-1, -1, -1, 0, 0, 1, 1, 1};
+  private static final int[] SQUARE_COL = {-1, 0, 1, -1, -1, -1, 0, 1};
   private static final int[] HEXAGONAL_ROW = {-1, -1, -1, 0, 0, 1};
-  private static final int[] TRIANGULAR_ROW = {-1, -1, -1, -1, -1, 0, 0, 0, 0, 1, 1, 1};
-  private static final int[] SQUARE_COL = {0, 0, -1, 1, -1, 1, -1, 1};
   private static final int[] HEXAGONAL_COL = {-1, 0, 1, -1, 1, 0};
+  private static final int[] TRIANGULAR_ROW = {-1, -1, -1, -1, -1, 0, 0, 0, 0, 1, 1, 1};
   private static final int[] TRIANGULAR_COL = {-2, -1, 0, 1, 2, -2, -1, 1, 2, -1, 0, 1};
   private static final int FIRST = 0;
   private static final int FULL = -1;
